b3d9f238f275c7b9c3e27b57666ace18112b941a
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-02-04 10:51:02 +0100

    Running simulation loop

--- Assets/Script/Grid/GridManager.cs
+++ Assets/Script/Grid/GridManager.cs
@@ -9,10 +9,24 @@ namespace GameOfLife
         [SerializeField] private int _rows;
         [SerializeField] private int _columns;
         [SerializeField] private GameObject _cellPrefab;
+        [SerializeField] private float _stepInterval;
+        [SerializeField] private bool _pause;
 
         private void Awake()
         {
-            _neighboursIndices = new int[8,2] { 
+            Init();
+            CreateCells();
+        }
+
+
+        private void Init()
+        {
+            _nextStepTime = Time.time;
+
+            _cells = new CellManager[_columns, _rows];
+            _transform = transform;
+
+            _neighboursIndices = new int[8, 2] {
                 {-1,-1},
                 {-1, 0},
                 {-1, 1},
@@ -24,33 +38,61 @@ namespace GameOfLife
                 { 1, 0},
                 { 1, 1}
             };
-            _cells = new GameObject[_rows, _columns];
-            _transform = transform;
-            CreateCells();
         }
 
         private void CreateCells()
         {
             Vector2 cellScale = new Vector2(_transform.localScale.x / _columns, _transform.localScale.y / _rows);
-            Vector2 offset = new Vector2(_transform.position.x - _transform.localScale.x / 2 + cellScale.x / 2, _transform.position.x + _transform.localScale.y /2 - cellScale.y / 2);
+            Vector2 offset = new Vector2(_transform.position.x - _transform.localScale.x / 2 + cellScale.x / 2, _transform.position.y + _transform.localScale.y /2 - cellScale.y / 2);
+
 
             for (int i = 0; i < _cells.GetLength(0); i++)
             {
                 for (int j = 0; j < _cells.GetLength(1); j++)
                 {
                     
-                    GameObject cell = Instantiate(_cellPrefab, _transform);
+                    GameObject cell = Instantiate(_cellPrefab, _transform) ;
                     cell.transform.localScale = cellScale / (Vector2)_transform.localScale;
                     cell.transform.position = _transform.position + (Vector3) offset + Vector3.right * (cellScale.x * i) + Vector3.down * (cellScale.y * j);
-                    _cells[i, j] = cell;
-                    
+                    _cells[i, j] = cell.GetComponent<CellManager>();
                 }
             }
         }
 
         private void Update()
         {
-            Debug.Log(CountAliveNeighbours(7, 7));
+            if(!_pause && Time.time > _nextStepTime)
+            {
+                _nextStepTime = Time.time + _stepInterval;
+                NextStep();
+            }
+        }
+
+        private void NextStep()
+        {
+            for (int i = 0; i < _cells.GetLength(0); i++)
+            {
+                for (int j = 0; j < _cells.GetLength(1); j++)
+                {
+                    UpdateCell(i, j);
+                }
+            }
+        }
+
+        private void UpdateCell(int x, int y) {
+            int neighboursCount = CountAliveNeighbours(x, y);
+            CellManager cellManager = _cells[x,y].GetComponent<CellManager>();
+
+            if (neighboursCount == 3)
+            {
+                cellManager.IsAlive = true;
+            } else if( neighboursCount == 2)
+            {
+                // change nothing
+            } else
+            {
+                cellManager.IsAlive = false;
+            }
         }
 
         void OnDrawGizmos()
@@ -63,10 +105,9 @@ namespace GameOfLife
         private int CountAliveNeighbours(int x, int y)
         {
             int aliveCount = 0;
-            foreach(GameObject neighbour in GetNeighbours(x, y))
+            foreach(CellManager neighbour in GetNeighbours(x, y))
             {
-                CellManager cell = neighbour.GetComponent<CellManager>();
-                if (cell != null && cell.IsAlive)
+                if (neighbour != null && neighbour.IsAlive)
                 {
                     aliveCount++;
                 }
@@ -75,19 +116,19 @@ namespace GameOfLife
             return aliveCount;
         }
 
-        private List<GameObject> GetNeighbours(int x, int y)
+        private List<CellManager> GetNeighbours(int x, int y)
         {
-            List<GameObject> neighbours = new List<GameObject>();
+            List<CellManager> neighbours = new List<CellManager>();
             
             for (int i = 0; i < _neighboursIndices.GetLength(0); i++)
             {
-                AddNeighbourToList(neighbours, x + _neighboursIndices[i,0], y + +_neighboursIndices[i, 1]);
+                AddNeighbourToList(neighbours, x + _neighboursIndices[i,0], y + _neighboursIndices[i, 1]);
             }
 
             return neighbours;
         }
 
-        private void AddNeighbourToList(List<GameObject> neighbours, int x, int y)
+        private void AddNeighbourToList(List<CellManager> neighbours, int x, int y)
         {
             if(IsNeighbourExist(x, y))
             {
@@ -97,12 +138,13 @@ namespace GameOfLife
 
         private bool IsNeighbourExist(int x, int y)
         {
-            return !(x < 0 || x > _columns || y < 0 || y > _rows);
+            return !(x < 0 || x > _columns-1 || y < 0 || y > _rows-1);
         }
 
+        private float _nextStepTime;
         private int[,] _neighboursIndices;
         private Transform _transform;
-        private GameObject[,] _cells;
+        private CellManager[,] _cells;
     }
 }
 
